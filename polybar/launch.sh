b1b344ce5412a80138bfe898b3c8ceaf12ead56c
eze8789 <ezequiel.reyna@gmail.com>
2023-01-08 15:04:56 -0300

    add support for multiple monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,15 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+    MONITOR=$m polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+  done
+else
+    echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+    MONITOR=$m polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+fi
 
+# Launch bar1 and bar2
 echo "Bars launched..."
